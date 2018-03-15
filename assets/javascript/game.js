ef13d16e948c860eb29523305d161cf8287a8fcd
EmmaNem <emmabv1@gmail.com>
2018-03-14 19:34:33 -0700

    basic code

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAlopL725AQmgqkvwsjJYdXtuZgkebAPdM",
+    authDomain: "rock-paper-scissors-502d7.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-502d7.firebaseio.com",
+    projectId: "rock-paper-scissors-502d7",
+    storageBucket: "rock-paper-scissors-502d7.appspot.com",
+    messagingSenderId: "890900020473"
+  };
+firebase.initializeApp(config);
